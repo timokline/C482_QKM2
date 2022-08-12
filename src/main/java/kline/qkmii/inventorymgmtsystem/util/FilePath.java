94a1a01dbc8602cc5f2dbd9a9594e9c7ae99bc1e
Timothy Albert Kline <snqy.trtyl+dev@proton.me>
2022-08-12 13:05:34 -0800

    Added skeleton of fxml into add parts controller class; various fx:id and handle methods created. added utility classes

---
+++ src/main/java/kline/qkmii/inventorymgmtsystem/util/FilePath.java
@@ -0,0 +1,9 @@
+package kline.qkmii.inventorymgmtsystem.util;
+
+//Utility class for fxml scene file paths.
+public final class FilePath {
+    //private FilePath() {} //Cannot be instantiated.
+    public static final String PARTS_SCENE = "view/add-part-form.fxml";
+    public static final String MAIN_MENU = "view/main-form.fxml";
+    public static final String PRODUCT_SCENE = "view/add-product-form.fxml";
+}
---
+++ src/main/java/kline/qkmii/inventorymgmtsystem/util/SceneManager.java
@@ -0,0 +1,25 @@
+package kline.qkmii.inventorymgmtsystem.util;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import kline.qkmii.inventorymgmtsystem.InvMgmtSysMain;
+
+import java.io.IOException;
+
+public abstract class SceneManager {
+    Stage stage; //private
+    Parent scene; //private
+
+    public void switchScenes(ActionEvent event, String location) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(InvMgmtSysMain.class.getResource(location));
+        scene = new Scene(fxmlLoader.load()).getRoot();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow(); //Might be a bug w/ type-casting
+        stage.setTitle("YAY!");
+        stage.setScene(scene.getScene());
+        stage.show();
+    }
+}
