ed78e7d4fe254dfb9787bc18c69aa1c572c3004c
Timothy Albert Kline <snqy.trtyl+dev@proton.me>
2022-09-15 04:06:58 -0800

    Removed unused FilePath constants

--- src/main/java/kline/qkmii/inventorymgmtsystem/util/FilePath.java
+++ src/main/java/kline/qkmii/inventorymgmtsystem/util/FilePath.java
@@ -2,12 +2,10 @@ package kline.qkmii.inventorymgmtsystem.util;
 
 //Utility class for fxml scene file paths.
 public final class FilePath {
-  private FilePath() {
-  } //Cannot be instantiated.
-
   public static final String MAIN_MENU = "view/main-form.fxml";
   public static final String PARTS_FORM_SCENE = "view/part-form.fxml";
-  //public static final String MODIFY_PARTS_SCENE = "view/modify-part-form.fxml";
-  public static final String ADD_PRODUCT_SCENE = "view/add-product-form.fxml";
-  public static final String MODIFY_PRODUCT_SCENE = "view/modify-product-form.fxml";
+  public static final String PRODUCTS_FORM_SCENE = "view/product-form.fxml";
+
+  private FilePath() {
+  }
 }
