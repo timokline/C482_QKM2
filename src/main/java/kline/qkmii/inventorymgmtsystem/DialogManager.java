3f23bbee2b41955d04511a5b6e0c0aaab9233990
Timothy Albert Kline <snqy.trtyl+dev@proton.me>
2022-09-18 19:08:11 -0800

    Refactored DialogManager. removed instantiation of the class

--- src/main/java/kline/qkmii/inventorymgmtsystem/DialogManager.java
+++ src/main/java/kline/qkmii/inventorymgmtsystem/DialogManager.java
@@ -3,19 +3,13 @@ package kline.qkmii.inventorymgmtsystem;
 import javafx.scene.control.Alert;
 
 public final class DialogManager {
-  private static DialogManager instance = null;
-
   private DialogManager() {
-  }
-
-  public synchronized static DialogManager getInstance() {
-    return (instance == null) ? (instance = new DialogManager()) : instance;
   }
 
   public static void SearchError() {
     Alert alert = new Alert(Alert.AlertType.ERROR);
     alert.setTitle("Search Error");
-    alert.setContentText("No such item was found in the table.");
+    alert.setContentText("Cannot find item in table.");
     alert.showAndWait();
   }
 
@@ -28,21 +22,22 @@ public final class DialogManager {
 
   public static void ProductDeletionError() {
     Alert alert = new Alert(Alert.AlertType.ERROR);
-    alert.setTitle("Deletion Error");
-    alert.setContentText("Product has parts associated with it. Modify item to remove parts.");
+    alert.setTitle("Delete Product");
+    alert.setContentText("Cannot delete Product. Contains associated parts.");
     alert.showAndWait();
   }
 
   public static Alert ProductDeletionConfirmation() {
     Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-    alert.setTitle("Confirm Deletion");
+    alert.setTitle("Delete Product");
+    alert.setHeaderText("Confirm Deletion?");
     alert.setContentText("Are you sure you would like to delete this product?");
     return alert;
   }
 
   public static void ProductDeletionInfo() {
     Alert alert = new Alert(Alert.AlertType.INFORMATION);
-    alert.setTitle("Deletion Canceled");
+    alert.setTitle("Delete Product");
     alert.setHeaderText(null);
     alert.setContentText("Product was not deleted.");
     alert.showAndWait();
@@ -50,21 +45,23 @@ public final class DialogManager {
 
   public static void PartDeletionError() {
     Alert alert = new Alert(Alert.AlertType.ERROR);
-    alert.setTitle("Deletion Error");
+    alert.setTitle("Delete Part");
+    alert.setHeaderText("Deletion Error");
     alert.setContentText("FIXME: This should not be seen");
     alert.showAndWait();
   }
 
   public static Alert PartDeletionConfirmation() {
     Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-    alert.setTitle("Confirm Deletion");
+    alert.setTitle("Delete Part");
+    alert.setHeaderText("Confirm Deletion?");
     alert.setContentText("Are you sure you would like to delete this part?");
     return alert;
   }
 
   public static void PartDeletionInfo() {
     Alert alert = new Alert(Alert.AlertType.INFORMATION);
-    alert.setTitle("Deletion Canceled");
+    alert.setTitle("Delete Part");
     alert.setHeaderText(null);
     alert.setContentText("Part was not deleted.");
     alert.showAndWait();
@@ -72,21 +69,23 @@ public final class DialogManager {
 
   public static void PartRemovalError() {
     Alert alert = new Alert(Alert.AlertType.ERROR);
-    alert.setTitle("Removal Error");
+    alert.setTitle("Remove Part");
+    alert.setHeaderText("Removal Error");
     alert.setContentText("FIXME: This should not be seen");
     alert.showAndWait();
   }
 
   public static Alert PartRemovalConfirmation() {
     Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-    alert.setTitle("Confirm Removal");
+    alert.setTitle("Remove Part");
+    alert.setHeaderText("Confirm Removal?");
     alert.setContentText("Are you sure you would like to remove this part?");
     return alert;
   }
 
   public static void PartRemovalInfo() {
     Alert alert = new Alert(Alert.AlertType.INFORMATION);
-    alert.setTitle("Removal Canceled");
+    alert.setTitle("Remove Part");
     alert.setHeaderText(null);
     alert.setContentText("Part was not removed.");
     alert.showAndWait();
