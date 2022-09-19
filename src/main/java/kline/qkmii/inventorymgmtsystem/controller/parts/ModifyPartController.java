4ac5d04db27edbe66da92de8ad2bfab444e15a61
Timothy Albert Kline <snqy.trtyl+dev@proton.me>
2022-09-18 22:26:33 -0800

    Added Javadocs to ModifyPartController.java

--- src/main/java/kline/qkmii/inventorymgmtsystem/controller/parts/ModifyPartController.java
+++ src/main/java/kline/qkmii/inventorymgmtsystem/controller/parts/ModifyPartController.java
@@ -1,3 +1,12 @@
+/*
+ * FNAM: ModifyPartController.java
+ * DESC: Derived controller class of PartsController; modify
+ * AUTH: Timothy Albert Kline
+ *
+ * UPDT: 18 Sept 2022
+ * VERS: 1.0
+ * COPR: N/A
+ */
 package kline.qkmii.inventorymgmtsystem.controller.parts;
 
 import javafx.event.ActionEvent;
@@ -8,10 +17,30 @@ import kline.qkmii.inventorymgmtsystem.model.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/** Controller class to handle modification of an existing <code>Part</code>.
+ * Inherits from the base class <code>PartsController</code>. Given a <code>Part</code> object,
+ * copies information about part to be injected into the corresponding FXML fields.
+ * Upon a save event, calls a validation method and updates the part in <code>Inventory</code>.
+ * @author Timothy Albert Kline
+ * @version 1.0
+*/
 public class ModifyPartController extends PartsController {
-
+  /**
+   * index in <code>Inventory</code> of part being modified.
+   */
   private int currPartIndex;
 
+  /** Controller constructor specifying a part to modify.
+   * Provided a <code>Part</code> object, calls <code>fetchPartInfo</code> to store information about the part.
+   * Creates a new <code>PartFactory</code> using the part to be updated.
+   * Initializes <code>formLabelText</code> to be pre-injected into FXML
+   *
+   * @param selectedPart the part to be modified.
+   * @see PartsController#formLabelText
+   * @see #fetchPartInfo(Part)
+   * @see #initialize(URL, ResourceBundle)
+   * @see PartFactory#PartFactory(Part)
+   */
   public ModifyPartController(Part selectedPart) {
     super();
     fetchPartInfo(selectedPart);
@@ -19,6 +48,17 @@ public class ModifyPartController extends PartsController {
     formLabelText = "Modify Part";
   }
 
+  /** Handles saving a modified part.
+   * Calls <code>validateInput()</code> to verify user inputs are correct
+   * before updating part in <code>Inventory</code>. Returns to menu view if
+   * succeeds.
+   *
+   * @param event save button click. To redirect back to menu.
+   * @throws Exception error log from <code>validateInputs()</code>.
+   * Prevents update of part in <code>inventory</code>
+   * @see PartsController#validateInputs()
+   * @see SceneManager#returnToMenu(ActionEvent)
+   */
   @FXML
   public void handleSaveBtnEvent(ActionEvent event) throws Exception {
     try {
@@ -31,8 +71,21 @@ public class ModifyPartController extends PartsController {
     }
   }
 
-  //FOR PART G:
-  //IMPLEMENT COMMAND DESIGN PATTERN TO CALL EACH ACCESSOR METHOD PER PROPERTY/FIELD OF PART
+  /** Stores information about the part being modified.
+   * Provided a <code>Part</code> object, copies part information into appropriate instance variables.
+   * Stores the index in <code>Inventory</code> of the part.
+   *
+   * @param selectedPart the part to be modified.
+   * @see #currPartIndex
+   * @see PartsController#currPartID
+   * @see PartsController#currPartName
+   * @see PartsController#currPartStock
+   * @see PartsController#currPartPrice
+   * @see PartsController#currMinParts
+   * @see PartsController#currMaxParts
+   * @see PartsController#currPartSrc
+   * @see PartsController#currPartType
+   */
   private void fetchPartInfo(Part selectedPart) {
     currPartID = selectedPart.getId();
     currPartName = selectedPart.getName();
@@ -50,6 +103,17 @@ public class ModifyPartController extends PartsController {
     currPartIndex = Inventory.getAllParts().indexOf(selectedPart);
   }
 
+  /** Injects <code>Part</code> information into FXML fields.
+   * The current <code>Part</code>'s information fetched during the constructor
+   * instantiation is injected into the loaded FXML fields. <code>Radio Button</code>s
+   * of view are updated per source of the current part.
+   *
+   * @param url url
+   * @param resourceBundle resource bundle
+   * @see #ModifyPartController(Part)
+   * @see PartsController
+   * @see PartsController#initialize(URL, ResourceBundle)
+   */
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
     idTF.setText(String.valueOf(currPartID));
