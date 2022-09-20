d51b3f29e0bbbb37367e2ce037305b19374e091e
Timothy Albert Kline <snqy.trtyl+dev@proton.me>
2022-09-19 18:43:11 -0800

    Added Javadocs to InHouse.java and OutSourced.java.

--- src/main/java/kline/qkmii/inventorymgmtsystem/model/InHouse.java
+++ src/main/java/kline/qkmii/inventorymgmtsystem/model/InHouse.java
@@ -1,17 +1,50 @@
+/*
+ * FNAM: InHouse.java
+ * DESC: Derived model class of Part; InHouse
+ * AUTH: Timothy Albert Kline
+ *
+ * UPDT: 19 Sept 2022
+ * VERS: 1.0
+ * COPR: N/A
+ */
 package kline.qkmii.inventorymgmtsystem.model;
 
+/** Subclass for an in-house part.
+ * Inherits from the base class <code>Part</code>.
+ * @author Timothy Albert Kline
+ * @version 1.0
+ * @see Part
+ */
 public class InHouse extends Part {
   private int machineId;
 
+  /** Model constructor specifying part information.
+   * Calls <code>super</code> on part information.
+   * Stores the machine code that made the part.
+   *
+   * @param id part id
+   * @param name part name
+   * @param price part price
+   * @param stock current part inventory level
+   * @param min minimum part inventory level
+   * @param max maximum part inventory level
+   * @param machineId machine code of part
+   */
   public InHouse(int id, String name, double price, int stock, int min, int max, int machineId) {
     super(id, name, price, stock, min, max);
     this.machineId = machineId;
   }
 
+  /**
+   * @return the machine code
+   */
   public int getMachineId() {
     return this.machineId;
   }
 
+  /**
+   * @param machineId the code to set
+   */
   public void setMachineId(int machineId) {
     this.machineId = machineId;
   }
--- src/main/java/kline/qkmii/inventorymgmtsystem/model/OutSourced.java
+++ src/main/java/kline/qkmii/inventorymgmtsystem/model/OutSourced.java
@@ -1,17 +1,50 @@
+/*
+ * FNAM: OutSourced.java
+ * DESC: Derived model class of Part; OutSourced
+ * AUTH: Timothy Albert Kline
+ *
+ * UPDT: 19 Sept 2022
+ * VERS: 1.0
+ * COPR: N/A
+ */
 package kline.qkmii.inventorymgmtsystem.model;
 
+/** Subclass for an outsourced part.
+ * Inherits from the base class <code>Part</code>.
+ * @author Timothy Albert Kline
+ * @version 1.0
+ * @see Part
+ */
 public class OutSourced extends Part {
   private String companyName;
 
+  /** Model constructor specifying part information.
+   * Calls <code>super</code> on part information.
+   * Stores the name of the company that provides part.
+   *
+   * @param id part id
+   * @param name part name
+   * @param price part price
+   * @param stock current part inventory level
+   * @param min minimum part inventory level
+   * @param max maximum part inventory level
+   * @param companyName company name of part
+   */
   public OutSourced(int id, String name, double price, int stock, int min, int max, String companyName) {
     super(id, name, price, stock, min, max);
     this.companyName = companyName;
   }
 
+  /**
+   * @return the company name
+   */
   public String getCompanyName() {
     return this.companyName;
   }
 
+  /**
+   * @param companyName the name to set
+   */
   public void setCompanyName(String companyName) {
     this.companyName = companyName;
   }
